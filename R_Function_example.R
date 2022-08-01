4048516839d010477e6cc27270bcf6530ac46a11
hyesung6 <k_hyesung6@naver.com>
2022-08-01 23:55:43 +0900

    R study

--- R_Function_example.R
+++ R_Function_example.R
@@ -1,3 +1,4 @@
+# 함수 예제
 sum_ <- function(a, b) {
   the_sum_ <- a + b
   return(the_sum_)
@@ -6,3 +7,21 @@ sum_ <- function(a, b) {
 sum_(1, 2)
 
 sum_(100, 200)
+
+
+# if 문 예제 
+
+bread <- 1000
+
+
+if ( bread >= 1000) {
+  print('bread sold out!')
+} else {
+  print('bread still here ...')
+}
+
+if ( bread > 20) {
+  print('You sold a lot!')
+} else {
+  print('Not enough for today')  
+}
